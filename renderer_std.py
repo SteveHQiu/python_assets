3a113734da365fdf59072490be9b57ac823206a4
EndorphinSponge <hqiu1@ualberta.ca>
2022-07-14 23:18:35 -0400

    Generated new XML with more images

--- renderer_std.py
+++ renderer_std.py
@@ -372,9 +372,12 @@ def renderImage(node: OENodePoint, front: bool, level: str, renderer: StandardRe
         if level == "entry":
             return "" # Shouldn't have image as entry point, unless there's a specific function (e.g., name this picture)
         elif level == "direct_child":
-            # img_name = genImageName(node)
+            img_name = genImageName(node)
+            print(img_name)
             return f"<li>{getFxName()}, front: {front}, level: {level}</li>\n"
         elif level == "sibling":
+            img_name = genImageName(node)
+            print(img_name)
             return f"<li>{getFxName()}, front: {front}, level: {level}</li>\n"
 
 
